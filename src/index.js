e8e1e5366f04d97b6d371b58a96d63932fabc7c3
shaktiman <codeshaktiman@gmail.com>
2022-02-26 18:09:31 +0100

    End of structuring folders

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import './app/layout/styles.css';
+import ReactDOM from 'react-dom';
+import './app/layout/styles.css'
 import App from './app/layout/App';
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
 
-
-//  Hotmodule replacement
-const rootEle = document.getElementById("root");
-
-function render() {
-  ReactDOM.render(<App />, rootEle);
-}
-
-if (module.hot) {
-  module.hot.accept("./app/layout/App", function () {
-    setTimeout(render);
-  });
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
